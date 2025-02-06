793ef5f0e5e1d11a2c455e2352cadac3c57e0918
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-02-06 21:56:43 +0530

    Top bar and navbar

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
+
+
+const Header = () => {
+  return (
+    <header>
+      {/* Top Bar*/}
+      <Topbar/>
+      {/* Navbar */}
+      <Navbar/>
+      {/*Cart*/}
+    </header>
+  )
+}
+
+export default Header
