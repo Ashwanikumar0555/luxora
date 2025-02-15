330507734e8720d1c66061f89188c2d7974b81e3
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-02-15 22:02:55 +0530

    added toggle to navbar

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,75 +1,137 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { HiOutlineUser, HiOutlineShoppingBag, } from "react-icons/hi2";
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineUser, HiOutlineShoppingBag } from "react-icons/hi2";
 import { HiBars3BottomRight } from "react-icons/hi2";
-import SearchBar from './SearchBar';
+import SearchBar from "./SearchBar";
+import CartDrawer from "../Layout/CartDrawer";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import styled from 'styled-components';
+import LuxoraButton from "../LuxoraButton";
+
 
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+
+  const toggleNavDrawer = () => {
+    setNavDrawerOpen(!navDrawerOpen);
+  };
+
+  const toggleCartDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
-   <>
-   <nav className="container mx-auto flex item-center justify-between py-4 px-6">
-   
-   {/*  Left - Logo */}
-   
-   <div>
-    <Link to="/" className="text-2xl font-medium">
-    Luxora
-    </Link>
-   </div>
+    <>
+      <nav className="container mx-auto flex item-center justify-between py-4 px-6">
+        {/*  Left - Logo */}
 
-   {/* Center - Navigation  Links */}
-    <div className="hidden md:flex space-x-6">
-        <Link to="#"
-         className="text-gray-700 hover:text-black text-sm font-medium uppercase"
-        >
-       Men
-        </Link>
+        <div>
+          <Link to="/" className="text-2xl font-medium">
 
-        <Link to="#"
-         className="text-gray-700 hover:text-black text-sm font-medium uppercase"
-        >
-       Women
-        </Link>
+            {/* LUXORA BUTTON BATMAN  */}
+            <LuxoraButton/>
+            {/* LUXORA BUTTON BATMAN  */}
+            
+          </Link>
+        </div>
 
-        <Link to="#"
-         className="text-gray-700 hover:text-black text-sm font-medium uppercase"
-        >
-       Top Wear
-        </Link>
+        {/* Center - Navigation  Links */}
+        <div className="hidden md:flex space-x-6">
+          <Link
+            to="#"
+            className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+          >
+            Men
+          </Link>
 
-        <Link to="#"
-         className="text-gray-700 hover:text-black text-sm font-medium uppercase"
-        >
-       Bottom Wear
-        </Link>
-    </div>
+          <Link
+            to="#"
+            className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+          >
+            Women
+          </Link>
+
+          <Link
+            to="#"
+            className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+          >
+            Top Wear
+          </Link>
+
+          <Link
+            to="#"
+            className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+          >
+            Bottom Wear
+          </Link>
+        </div>
+
+        {/* Right - Icons */}
+
+        <div className=" flex items-center space-x-4 ">
+          <Link to="/profile" className="hover:text-color-black">
+            <HiOutlineUser className="h-6 w-6 text-gray-700" />
+          </Link>
+          <button
+            onClick={toggleCartDrawer}
+            className="relative hover:text-black"
+          >
+            <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
+            <span className="absolute -top-1  bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5">
+              4
+            </span>
+          </button>
+
+          {/* Search */}
+          <div className="overflow-hidden"></div>
+          <SearchBar />
+          <button onClick={toggleNavDrawer} className="md:hidden">
+            <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
+          </button>
+        </div>
+      </nav>
+      <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+
+      {/* Mobile Navigation */}
+
+      <div
+        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${
+          navDrawerOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex justify-end p-4">
+          <button onClick={toggleNavDrawer}>
+            <IoMdClose className="h-6 w-6 text-gray-600 " />
+          </button>
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4">Menu</h2>
+          <nav className="space-y-4">
+            <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
+            Men 
+            </Link>
+           
+
+            <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
+            Women
+            </Link>
+
+            <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
+            Top Wear
+            </Link>
+
+            <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
+            Bottom Wear 
+            </Link>
 
 
-   {/* Right - Icons */}
-
-   <div className=" flex items-center space-x-4 ">
-    <Link to="/profile" className="hover:text-color-black">
-    <HiOutlineUser className="h-6 w-6 text-gray-700"/>
-    </Link>
-    <button className="relative hover:text-black">
-   <HiOutlineShoppingBag className="h-6 w-6 text-gray-700"/>
-   <span className="absolute -top-1  bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5">
-    4
-   </span>
-    </button>
-
-    {/* Search */}
-    <div className="overflow-hidden">
-        
-    </div>
-     <SearchBar/>
-    <button className="md:hidden">
-        <HiBars3BottomRight className="h-6 w-6 text-gray-700"/>
-    </button>
-       </div>
-   </nav>
+          </nav>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
