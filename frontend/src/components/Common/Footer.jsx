69f4538b0d5c4ad34150b0316bf316a59d5eccf9
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-02-15 22:09:03 +0530

    🔗 Added responsive Footer component with newsletter, shop links, support, and social media icons

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { TbBrandMeta } from "react-icons/tb";
+import { IoLogoInstagram } from "react-icons/io";
+import { RiTwitterXLine } from "react-icons/ri";
+import { FiPhoneCall } from "react-icons/fi";
+
 
 const Footer = () => {
   return (
@@ -83,7 +88,9 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
+
             {/* Follow Us  */}
+
             <div>
               <h3 className="text-lg text-gray-800 mb-4 "> Follow Us </h3>
               <div className=" flex items-center space-x-4 mb-6 ">
@@ -91,13 +98,39 @@ const Footer = () => {
                 href="https:facebook.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="hover:text-gray-300"
+                className="hover:text-gray-500"
                 >
-                  
+                  <TbBrandMeta className="h-5 w-5"/>
+                </a>
+                <a 
+                href="https:facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-500"
+                >
+                  < IoLogoInstagram className="h-5 w-5"/>
+                </a>
+                <a 
+                href="https:facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-500"
+                >
+                 <RiTwitterXLine  className="h-4 w-4"/>
                 </a>
               </div>
+              <p className="text-gray-500 "> Call Us </p>
+              <p>
+              <FiPhoneCall  className="inline-block mr-2" />
+              0123-456-789
+              </p>
             </div>
-        </div>
+           </div>
+
+           {/* Footer Bottom   */}
+           <div className="container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6 ">
+            <p className="text-gray-500 text-sm tracking-tighter text-center "> © 2025, CompileTab. All Rights Reserved.</p>
+           </div>
 
     </footer>
   )
