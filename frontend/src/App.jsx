be08b17b22cc3bc617000db495220b5d05f6e523
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-03-01 23:35:18 +0530

    🎨 Updated App and Navbar components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,74 +1,11 @@
-// import React from 'react'
-// import { BrowserRouter,Routes,Route } from 'react-router-dom'
-// import UserLayout from './components/Layout/UserLayout'
-// import Home from "./pages/Home";
-// import { Toaster } from "sonner";
-// import Login from "./pages/Login"; 
-// import Register from './pages/Register';
-// import Profile from './pages/Profile'
-// import CollectionPage from './pages/CollectionPage';
-// import ProductDetails from './components/Products/ProductDetails';
-// import Checkout from './components/Cart/Checkout';
-// import OrderConfirmationPage from "./pages/OrderConfirmationPage"
-// import OrderDetailsPage from './pages/OrderDetailsPage';
-// import MyOrdersPage from './pages/MyOrdersPage';
-// import AdminLayout from './components/Admin/AdminLayout';
-// import AdminHomePage from './pages/AdminHomePage';
-// import UserManagement from './components/Admin/UserManagement';
-// import ProductManagement from './components/Admin/ProductManagement';
-// import EditProductPage from './components/Admin/EditProductPage';
-// import OrderManagement from './components/Admin/OrderManagement';
-
-
-// const App = () => {
-//   return (
-//   <BrowserRouter>
-//   <Toaster position="top-right" />
-//   <Routes>
-//     <Route path="/" element={<UserLayout/>}>
-//     <Route index element={<Home/>} />
-//     <Route path="login" element={<Login/>}/>
-//     <Route path="register" element={<Register/>}/>
-//      <Route path="profile" element={<Profile/>}/>
-//      <Route path="collections/:collection" element={<CollectionPage/>}/>
-//      <Route path="product/:id" element={<ProductDetails/>}/>
-//      <Route path="checkout" element={<Checkout/>}/>
-//      <Route path="order-confirmation" element={<OrderConfirmationPage/>}/>
-//      <Route path="order/:id" element={<OrderDetailsPage/>}/>
-//      <Route path="my-orders" element={<MyOrdersPage/>}/>
-     
-//     </Route>
-
-//     {/* Admin Routes */}
-
-//     <Route path="/admin" element={<AdminLayout/>}> {/* Admin Layout */}
-//     <Route index element={<AdminHomePage/>}/>
-//     <Route path="users" element={<UserManagement/>}/>
-//     <Route path="products" element={<ProductManagement/>}/>
-//     <Route path="products/:id/edit" element={<EditProductPage/>}/>
-//     <Route path="orders" element={<OrderManagement/>}/>
-//     </Route>
-//   </Routes>
-//   </BrowserRouter>
-//   );
-// };
-
-
-
-// export default App  
-
-          
-
-
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
+import LuxoraLoader from "./components/Loader/LuxoraLoader";
 import UserLayout from "./components/Layout/UserLayout";
 import AdminLayout from "./components/Admin/AdminLayout";
 
 // User Pages
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -81,7 +18,6 @@ import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrdersPage from "./pages/MyOrdersPage";
 
 // Admin Pages
-
 import AdminHomePage from "./pages/AdminHomePage";
 import UserManagement from "./components/Admin/UserManagement";
 import ProductManagement from "./components/Admin/ProductManagement";
@@ -90,36 +26,41 @@ import OrderManagement from "./components/Admin/OrderManagement";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Toaster position="top-right" />
-      <Routes>
-        {/* User Routes */}
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="collections/:collection" element={<CollectionPage />} />
-          <Route path="product/:id" element={<ProductDetails />} />
-          <Route path="checkout" element={<Checkout />} />
-          <Route path="order-confirmation" element={<OrderConfirmationPage />} />
-          <Route path="order/:id" element={<OrderDetailsPage />} />
-          <Route path="my-orders" element={<MyOrdersPage />} />
-        </Route>
+   
+      <BrowserRouter>
+        <Toaster position="top-right" />
+        <LuxoraLoader />
+        <Routes>
+          {/* User Routes */}
+          <Route path="/" element={<UserLayout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="collections/:collection" element={<CollectionPage />} />
+            <Route path="product/:id" element={<ProductDetails />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="order-confirmation" element={<OrderConfirmationPage />} />
+            <Route path="order/:id" element={<OrderDetailsPage />} />
+            <Route path="my-orders" element={<MyOrdersPage />} />
+          </Route>
 
-        {/* Admin Routes */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminHomePage />} />
-          <Route path="users" element={<UserManagement />} />
-          <Route path="products" element={<ProductManagement />} />
-          <Route path="products/:id/edit" element={<EditProductPage />} />
-          <Route path="orders" element={<OrderManagement />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          {/* Admin Routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminHomePage />} />
+            <Route path="users" element={<UserManagement />} />
+            <Route path="products" element={<ProductManagement />} />
+            <Route path="products/:id/edit" element={<EditProductPage />} />
+            <Route path="orders" element={<OrderManagement />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    
   );
 };
 
 export default App;
+
+
 
 
